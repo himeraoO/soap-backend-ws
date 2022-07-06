e26e10d45e023ef621c1c672a6f4527cba46bab1
himeraoO <himer@bk.ru>
2022-07-06 22:28:04 +0300

    Update

--- src/main/java/com/example/soapbackendws/webservice/UsersEndpoint.java
+++ src/main/java/com/example/soapbackendws/webservice/UsersEndpoint.java
@@ -14,6 +14,7 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.regex.Pattern;
 
 @Endpoint
 public class UsersEndpoint {
@@ -68,11 +69,11 @@ public class UsersEndpoint {
         User user = userService.findByLogin(request.getLogin());
         ServiceStatus serviceStatus = new ServiceStatus();
         if (user == null ) {
-            serviceStatus.setStatusCode("FALSE");
+            serviceStatus.setStatus("FALSE");
             serviceStatus.setMessage("Content Not Available");
         } else {
             userService.delete(user);
-            serviceStatus.setStatusCode("SUCCESS");
+            serviceStatus.setStatus("SUCCESS");
             serviceStatus.setMessage("Content Deleted Successfully");
         }
         DeleteUserDetailsResponse response = new DeleteUserDetailsResponse();
@@ -85,29 +86,45 @@ public class UsersEndpoint {
     public AddUserDetailsResponse addUser(@RequestPayload AddUserDetailsRequest request) {
         AddUserDetailsResponse response = new AddUserDetailsResponse();
         ServiceStatus serviceStatus = new ServiceStatus();
-        User userBD = userService.findByLogin(request.getLogin());
-        if(userBD == null){
-            User user = new User();
-            user.setLogin(request.getLogin());
-            user.setUsername(request.getName());
-            user.setPassword(request.getPassword());
-            List<Long> listRolesIds = request.getRolesIds();
-            if ((listRolesIds == null) || (listRolesIds.isEmpty()) ){
-                user.setRoles(new HashSet<>());
-                userService.save(user);
-            }else {
-                userService.save(user, listRolesIds);
+
+        UserDetails userDetails = new UserDetails();
+        userDetails.setLogin(request.getLogin());
+        userDetails.setUsername(request.getName());
+        userDetails.setPassword(request.getPassword());
+
+        List<String> stringListException = verifyForm(userDetails);
+
+        if (stringListException.isEmpty()) {
+            User userBD = userService.findByLogin(request.getLogin());
+            if (userBD == null) {
+                User user = new User();
+                user.setLogin(request.getLogin());
+                user.setUsername(request.getName());
+                user.setPassword(request.getPassword());
+                List<Long> listRolesIds = request.getRolesIds();
+                if ((listRolesIds == null) || (listRolesIds.isEmpty())) {
+                    user.setRoles(new HashSet<>());
+                    userService.save(user);
+                } else {
+                    userService.save(user, listRolesIds);
+                }
+                response.setUserDetails(userDetails);
+                serviceStatus.setStatus("SUCCESS");
+                serviceStatus.setMessage("Content Added Successfully");
+                response.setServiceStatus(serviceStatus);
+            } else {
+                serviceStatus.setStatus("CONFLICT");
+                serviceStatus.setMessage("Content Already Available");
+                response.setServiceStatus(serviceStatus);
             }
-            UserDetails userDetails = new UserDetails();
-            userDetails.setLogin(user.getLogin());
-            userDetails.setUsername(user.getUsername());
-            response.setUserDetails(userDetails);
-            serviceStatus.setStatusCode("SUCCESS");
-            serviceStatus.setMessage("Content Added Successfully");
-            response.setServiceStatus(serviceStatus);
         }else {
-            serviceStatus.setStatusCode("CONFLICT");
-            serviceStatus.setMessage("Content Already Available");
+            for (String s :stringListException) {
+                ErrorSOAP errorSOAP = new ErrorSOAP();
+                errorSOAP.setMessage(s);
+                serviceStatus.getError().add(errorSOAP);
+            }
+            serviceStatus.setStatus("FALSE");
+            serviceStatus.setMessage("Received Data Verification Error. Added Failed");
             response.setServiceStatus(serviceStatus);
         }
         return response;
@@ -119,24 +136,68 @@ public class UsersEndpoint {
         User user = new User();
         ServiceStatus serviceStatus = new ServiceStatus();
         UserDetails userAllDetails = request.getUserDetails();
-        if(userService.findByLogin(userAllDetails.getLogin()) != null) {
-            user.setLogin(userAllDetails.getLogin());
-            user.setUsername(userAllDetails.getUsername());
-            user.setPassword(userAllDetails.getPassword());
-            List<Long> rolesIds = request.getRolesIds();
-            if ((rolesIds == null) || (rolesIds.isEmpty())) {
-                userService.update(user);
-            } else {
-                userService.update(user, rolesIds);
+        List<String> stringListException = verifyForm(userAllDetails);
+        if (stringListException.isEmpty()){
+            if(userService.findByLogin(userAllDetails.getLogin()) != null) {
+                user.setLogin(userAllDetails.getLogin());
+                user.setUsername(userAllDetails.getUsername());
+                user.setPassword(userAllDetails.getPassword());
+                List<Long> rolesIds = request.getRolesIds();
+                if ((rolesIds == null) || (rolesIds.isEmpty())) {
+                    userService.update(user);
+                } else {
+                    userService.update(user, rolesIds);
+                }
+                serviceStatus.setStatus("SUCCESS");
+                serviceStatus.setMessage("Content Updated Successfully");
+            }else {
+                serviceStatus.setStatus("FALSE");
+                serviceStatus.setMessage("Content Not Available. Updated Failed");
             }
-            serviceStatus.setStatusCode("SUCCESS");
-            serviceStatus.setMessage("Content Updated Successfully");
         }else {
-            serviceStatus.setStatusCode("FALSE");
-            serviceStatus.setMessage("Content Not Available. Updated Failed");
+            for (String s :stringListException) {
+                ErrorSOAP errorSOAP = new ErrorSOAP();
+                errorSOAP.setMessage(s);
+                serviceStatus.getError().add(errorSOAP);
+            }
+            serviceStatus.setStatus("FALSE");
+            serviceStatus.setMessage("Received Data Verification Error. Updated Failed");
         }
+
         UpdateUserDetailsResponse response = new UpdateUserDetailsResponse();
         response.setServiceStatus(serviceStatus);
         return response;
+    }
+
+    public List<String> verifyPassword(String password){
+        //password содержит букву в заглавном регистре и цифру
+        System.out.println(password);
+        List<String> stringListException = new ArrayList<>();
+        if(!Pattern.matches(".*[0-9].*", password)){
+            stringListException.add("Пароль должен содержать минимум 1 цифру");
+        }
+        if (!Pattern.matches(".*[A-ZА-ЯЁ].*", password)){
+            stringListException.add("Пароль должен содержать минимум 1 букву в заглавном регистре");
+        }
+        return stringListException;
+    }
+
+    public List<String> verifyForm(UserDetails userDetails){
+        List<String> stringListException = new ArrayList<>();
+        if(userDetails.getLogin().isEmpty() || userDetails.getLogin() == null){
+            stringListException.add("поле Login обязательно к заполнению");
+        }
+        if(userDetails.getUsername().isEmpty() || userDetails.getUsername() == null){
+            stringListException.add("поле Username обязательно к заполнению");
+        }
+        if(userDetails.getPassword().isEmpty() || userDetails.getPassword() == null){
+            stringListException.add("поле Password обязательно к заполнению");
+        }else {
+            List<String> passwordException = verifyPassword(userDetails.getPassword());
+            if (!passwordException.isEmpty()){
+                stringListException.addAll(passwordException);
+            }
+        }
+        return stringListException;
     }
 }
